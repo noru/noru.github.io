eb27ed9e17794002326303382b9147776a87f722
xiuxiuxiu <nooooru@gmail.com>
2019-06-18 19:24:02 +0800

    update demo

--- README.md
+++ README.md
@@ -2,6 +2,10 @@ My blog
 
 URL:
 
+[blog.xiuz.hu](https://blog.xiuz.hu)
+
+[meet.xiuz.hu](https://meet.xiuz.hu)
+
 [noru.github.io](https://noru.github.io)
-[blog.github.io](https://noru.github.io)
-[meet.github.io](https://noru.github.io)
+
+[noru.netlify.com](https://noru.netlify.com/)
--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -34,13 +34,13 @@ export default class NotFoundPage extends React.Component<any> {
           </Header>
           <Content>
             <GridWrapper>
-              <DemoWrapper src="/assets/demo/apm-dashboard/index.html" width={450}/>
-              <DemoWrapper src="/assets/demo/apm-dashboard/index.html#/dashboard2" width={450}/>
-              <DemoWrapper src="/assets/demo/apm-dashboard/index.html#/demo2" width={450}/>
-              <DemoWrapper src="/assets/demo/ruijin/index.html" width={450}/>
-              <DemoWrapper src="/assets/demo/dew/index.html#/FailureAnalysis" width={450}/>
-              <DemoWrapper src="/assets/demo/dew/index.html#/ProcessAnalysis" width={450}/>
-              <DemoWrapper isImage src="/assets/demo/appstore.png" link="https://apps.autodesk.com" width={450}/>
+              <DemoWrapper src="//blog.xiuz.hu/apm-dashboard" width={450} />
+              <DemoWrapper src="//blog.xiuz.hu/apm-dashboard#/dashboard2" width={450} />
+              <DemoWrapper src="//blog.xiuz.hu/apm-dashboard#/demo2" width={450} />
+              <DemoWrapper src="//blog.xiuz.hu/ct-monitor" width={450} />
+              <DemoWrapper src="//blog.xiuz.hu/dew-report#/FailureAnalysis" width={450} />
+              <DemoWrapper src="//blog.xiuz.hu/dew-report#/ProcessAnalysis" width={450} />
+              <DemoWrapper isImage src="/assets/demo/appstore.png" link="https://apps.autodesk.com" width={450} />
             </GridWrapper>
           </Content>
         </Wrapper>
