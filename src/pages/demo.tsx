0d85d130f9498cb811e6314c0af5b13272405942
drew <nooooru@gmail.com>
2019-12-08 13:44:32 +0800

    new blog

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -45,6 +45,7 @@ export default function DemoPage() {
         <Content>
           <Title style={{ textAlign: 'center' }}>Things I've been working on</Title>
           <GridWrapper>
+            <DemoWrapper src="//blog.xiuz.hu/snake" width={width} />
             <DemoWrapper src="//blog.xiuz.hu/visual-effects" width={width} />
             <DemoWrapper src="//blog.xiuz.hu/3-match/dist/index.html" width={width} />
             <DemoWrapper src="//blog.xiuz.hu/apm-dashboard" width={width} />
